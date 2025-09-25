c656e574054fb84a65e46b4224b40bc1ac5db11a
Loic Bernard <loic.bernard@example.com>
2025-09-25 13:17:53 +0200

    feat: Ajout de Vercel Analytics pour le tracking des utilisateurs

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import "./index.css";
 import "./styles/animations.css"; // Animations personnalisées
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );// Force redeploy Tue Sep 16 16:01:06 CEST 2025
